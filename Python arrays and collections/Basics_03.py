d64f3a1f8e4df24b5fb5c4ec0ae3172fd9149610
Sri Venkata Krishna Sourav V <krishnavitap@gmail.com>
2025-03-06 16:54:56 +0530

    Update Basics_03.py

--- Python arrays and collections/Basics_03.py
+++ Python arrays and collections/Basics_03.py
@@ -1,5 +1,5 @@
 import array as ar
 a = ar.array('i' , [1,2,3,4,5,6,7,8,9])
-a.append(10) # Tkes only element as the attribute
+a.append(10) # Takes only element as the attribute
 a.insert(10 , 11) # Takes the index as well as the element
 print(*a)
